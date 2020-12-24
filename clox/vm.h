005e166e8df1ef89a76bc6d8547197f03e689ace
Joshua Benuck <joshua_benuck@yahoo.com>
2020-12-24 00:15:11 -0800

    add call frames

--- clox/vm.h
+++ clox/vm.h
@@ -1,19 +1,23 @@
 #ifndef clox_vm_h
 #define clox_vm_h
 
-#include "chunk.h"
+#include "object.h"
 #include "table.h"
 #include "value.h"
 
-#define STACK_MAX 256
+#define FRAMES_MAX 64
+#define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
 
 typedef struct {
-    Chunk* chunk;
-    // Ideally would a be local variable in the VM
-    // Using a pointer to an integer rather than and index
-    // as it is faster than looking up an element in an
-    // array by index
+    ObjFunction* function;
     uint8_t* ip;
+    Value* slots;
+} CallFrame;
+
+typedef struct {
+    CallFrame frames[FRAMES_MAX];
+    int frameCount;
+
     Value stack[STACK_MAX];
     Value* stackTop;
     Table globals;
